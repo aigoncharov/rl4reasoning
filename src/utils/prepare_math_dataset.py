f95189a9b07b0b5d237edc92b064b6785dd82519
aigoncharov <andrey@goncharov.page>
2025-03-12 18:56:39 +0300

    Add seed for math split

--- src/utils/prepare_math_dataset.py
+++ src/utils/prepare_math_dataset.py
@@ -1,9 +1,7 @@
 from datasets import load_dataset
 
+
 def prepare_math_dataset():
     ds = load_dataset("open-r1/OpenR1-Math-220k", "default")
-    ds = ds["train"].train_test_split(test_size=0.2, shuffle=True)
+    ds = ds["train"].train_test_split(test_size=0.2, shuffle=True, seed=42)
     return ds["train"], ds["test"]
-
-
-
